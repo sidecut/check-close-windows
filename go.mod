ec57ba98da232bc2a8b6b894e909641593dc4097
Jim Raden <radenj@ccf.org>
2020-07-08 18:24:17 -0400

    Add web server w/ gzip

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/sidecut/check-close-windows
 
 go 1.14
+
+require github.com/labstack/echo/v4 v4.1.16
--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
 func main() {
-	
+	// Echo instance
+	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.Use(middleware.Gzip())
+	e.Use(middleware.Static("./dist"))
+
+	// Start server
+	e.Logger.Fatal(e.Start(":1323"))
 }
